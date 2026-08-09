1c870551f12e07f37534628954409e7d6dc81bd9
Ave626 <itped7@gmail.com>
2026-08-10 02:36:09 +0600

    chore: update migrations schema for quiz tables and course ownership

--- education_platform/migrations/env.py
+++ education_platform/migrations/env.py
@@ -1,15 +1,13 @@
 import asyncio
 from logging.config import fileConfig
 
+from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from alembic import context
-
 from app.infrastructure.database.database import settings
 from app.infrastructure.database.models import Base
-from app.infrastructure.database import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
